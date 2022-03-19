fd78637692b4b87666571d9d845bb8712c3a0dcf
kevinnammour <nammourkevin@gmail.com>
2022-03-19 23:02:30 +0200

    built api_url

--- server/apis/lbp-rate.php
+++ server/apis/lbp-rate.php
@@ -3,5 +3,7 @@
     $month = intval(date('m'));
     $day = intval(date('d'));
     $hour = intval(date('H'));
-    echo $year . ' ' . $month . ' ' . $day . ' ' . $hour;
+    $ver = $year . $month . $day . $hour;
+
+    $api_url = "https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t$ver";
 ?>
