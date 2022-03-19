8cf8dc3f94d1cb3451b412223d2827d56dd6b22b
kevinnammour <nammourkevin@gmail.com>
2022-03-19 15:23:03 +0200

    setting up env var

--- server/db/connection.php
+++ server/db/connection.php
@@ -1,7 +1,13 @@
 <?php
-    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    require '../vendor/autoload.php';
 
-    if (mysqli_connect_errno()) {
-        die("Connection failed");
-    }
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "./../");
+    $dotenv->load();
+    echo $_ENV['db_name'];
+
+    // $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    // if (mysqli_connect_errno()) {
+    //     die("Connection failed");
+    // }
 ?>
