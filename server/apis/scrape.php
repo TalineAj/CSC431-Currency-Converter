c2a593aba11d73628d38eab0824ac77701d3a231
kevinnammour <nammourkevin@gmail.com>
2022-03-19 17:58:50 +0200

    scrape regex done

--- server/apis/scrape.php
+++ server/apis/scrape.php
@@ -5,6 +5,12 @@
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_exec($ch);
+
+    $result = curl_exec($ch);
+
+    preg_match_all("/(.*?)\s(.*?)/", $result, $matches);
+    // /1\sUSD\sat\s\d\d,\d\d\d\sLBP/
+
+    print_r($matches);
     curl_close($ch);
 ?>
