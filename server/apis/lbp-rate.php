846f2e44e03c16816cf416bee24f97f0367ca768
kevinnammour <nammourkevin@gmail.com>
2022-03-19 23:32:40 +0200

    curl

--- server/apis/lbp-rate.php
+++ server/apis/lbp-rate.php
@@ -6,4 +6,11 @@
     $ver = $year . $month . $day . $hour;
 
     $api_url = "https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t$ver";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $api_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    echo $output;
+    curl_close($ch);
 ?>
