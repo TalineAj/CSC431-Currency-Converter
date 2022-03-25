82c81a0f6c4cef0c83c8c7173bbad52e38a91f87
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-03-25 22:40:27 +0200

    Action bar change -design

--- client/app/src/main/java/com/lau/front_end/MainActivity.java
+++ client/app/src/main/java/com/lau/front_end/MainActivity.java
@@ -2,13 +2,30 @@ package com.lau.front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean change = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //The code in line 23-->27 is not my own code, it was retrieved when I searched how to change the status bar color from:
+        //https://www.geeksforgeeks.org/how-to-change-the-color-of-status-bar-in-an-android-app/
+        //N.b: Button color and action bar were also changed and hidden respectively from the values folder
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.teal_200));
+        }
     }
+
 }
